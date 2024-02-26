7b0db000cf957218f9f5302fbb870ee5284ef140
kiplangat-victor <50839379+kiplangat-victor@users.noreply.github.com>
2024-02-26 13:42:36 +0300

    Delete banking-app directory

--- banking-app/src/main/java/net/javaguides/bankingapp/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package net.javaguides.bankingapp.repository;
-
-import net.javaguides.bankingapp.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-}
--- banking-app/src/main/java/net/javaguides/bankingapp/service/AccountService.java
+++
@@ -1,8 +0,0 @@
-package net.javaguides.bankingapp.service;
-
-import net.javaguides.bankingapp.dto.AccountDto;
-//import net.javaguides.bankingapp.entity.Account;
-
-public interface AccountService {
-    AccountDto createAccount(AccountDto accountDto);
-}
